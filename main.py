53b443e4003b943f63bb125ec287ac558091f5bb
leigao <leigao0910@gmail.com>
2021-10-31 19:06:15 -0400

    update

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # @Email    : leigao@umd.edu
 import pandas as pd
 import pickle
+from CCHP import CCHP_Model
 from CCHP_SDP import SDP_Model
 from util import record_cache, record_performance
 
@@ -16,20 +17,20 @@ def main(filename):
     infile_transition = open('sampling/markov_transition', 'rb')
     markov_transition = pickle.load(infile_transition)
     infile_transition.close()
-    # df_EP = pd.read_excel('sampling/cchp_data.xlsx', sheet_name='electricity_price', header=0, index_col=0)
-    # df_Temp = pd.read_excel('sampling/chp_data.xlsx', sheet_name='temperature', header=0, index_col=0)
     # Creat model
+    cchp = CCHP_Model()
+    cchp.add_subsystem()
     cchp_sdp = SDP_Model()
-    cchp_sdp.set_para_var()
+    cchp_sdp.set_para_var(cchp)
     cchp_sdp.create_model()
-    st_now, time_average = cchp_sdp.solve_sdp(markov_states, markov_transition, verbosity=0)
+    st_now, time_average = cchp_sdp.solve_sdp(cchp, markov_states, markov_transition, verbosity=0)
     # Record results
-    record_cache(filename + '_detailed', cchp_sdp)
-    record_performance(filename + '_overall', cchp_sdp, st_now, time_average)
+    record_cache(filename + '_detailed', cchp, cchp_sdp)
+    record_performance(filename + '_overall', cchp, cchp_sdp, st_now, time_average)
 
 
 if __name__ == '__main__':
-    main('24hr_4h0e0c')
+    main('24hr_2h0e0c')
     # infile_cache = open('result/24hr_10h0e0c_detailed_cache', 'rb')
     # cache_detailed = pickle.load(infile_cache)
     # infile_cache.close()
--- util.py
+++ util.py
@@ -6,38 +6,38 @@ import os
 import pickle
 
 
-def record_cache(filename, model):
+def record_cache(filename, cchp_model, sdp_model):
     outfile_cost = open('result/' + filename + '_cost', 'wb')
-    pickle.dump(model.sdp.cost, outfile_cost)
+    pickle.dump(sdp_model.sdp.cost, outfile_cost)
     outfile_cost.close()
     outfile_operation = open('result/' + filename + '_cache', 'wb')
     system_cache = {}
-    for sys in model.conv_system:
+    for sys in cchp_model.conv_system:
         system_cache.update({sys.name + str(sys.idx) + '_beta': sys.cache_beta})
         system_cache.update({sys.name + str(sys.idx) + '_alpha': sys.cache_alpha})
-    for sys in model.strg_system:
+    for sys in cchp_model.strg_system:
         system_cache.update({sys.name + str(sys.idx): sys.cache_state})
     pickle.dump(system_cache, outfile_operation)
     outfile_operation.close()
     print('Finish record cache data')
 
 
-def record_performance(file_in, model, st_now, time_count):
+def record_performance(file_in, cchp_model, sdp_model, st_now, time_count):
     if os.path.isfile('result/' + file_in):
         print("Output file for parameter already exists.")
         file_temp = 'result/' + file_in + ".old"
         print("Existing file has been copied to:", file_temp)
     file = open('result/' + file_in + '.txt', 'w')
     file.write("%%%%%%%%  Thermal System Information  %%%%%%%% \n")
-    for sys in model.conv_system:
+    for sys in cchp_model.conv_system:
         file.write(sys.name + str(sys.idx) + ' capacity: {0}, ramp : {1} '.format(sys.capacity, sys.ramp) + '\n')
     file.write("%%%%%%%%  Storage System Information  %%%%%%%% \n")
-    for sys in model.strg_system:
+    for sys in cchp_model.strg_system:
         file.write(sys.name + str(sys.idx) + ' capacity: {0}, ramp: {1}.'.format(sys.capacity, sys.ramp) + '\n')
     file.write("%%%%%%%%      Solver Information      %%%%%%%% \n")
 
     file.write("%%%%%%%%   Performance Information    %%%%%%%% \n")
-    file.write('The first stage cost is {0} '.format(model.sdp.cost[0, 0][st_now]) + '\n')
+    file.write('The first stage cost is {0} '.format(sdp_model.sdp.cost[0, 0][st_now]) + '\n')
     file.write('Average time consumption is {0} '.format(time_count) + '\n')
     file.close()
     print('Finish record performance data')
